2d0d15e4b9584b6598cbb3b69a1a492396c4b26f
Max Teegen <m_teegen@gmx.de>
2024-12-07 14:03:26 +0100

    solved day 7 part 2

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -9,6 +9,7 @@ type Output = Num;
 enum Op {
     Add,
     Mul,
+    Con,
 }
 
 impl std::fmt::Display for Op {
@@ -16,6 +17,28 @@ impl std::fmt::Display for Op {
         match self {
             Op::Add => write!(f, "+"),
             Op::Mul => write!(f, "*"),
+            Op::Con => write!(f, "|"),
+        }
+    }
+}
+
+impl Op {
+    fn apply(&self, a: Num, b: Num) -> Num {
+        match self {
+            Op::Add => a + b,
+            Op::Mul => a * b,
+            Op::Con => {
+                let b_digits = b.checked_ilog10().unwrap_or(0) + 1;
+                a * (10 as Num).pow(b_digits) + b
+            },
+        }
+    }
+
+    fn identity(self) -> Num {
+        match self {
+            Op::Add => 0,
+            Op::Mul => 1,
+            Op::Con => 0,
         }
     }
 }
@@ -38,14 +61,14 @@ fn one(input: &Input) -> Output {
             assert!(*i > 0);
             (Op::Add, *i)
         }).collect::<Vec<_>>();
-        if solvable(*res, &term, 1) {
+        if mul_solvable(*res, &term, 1) {
             answer += res;
         }
     }
     answer
 }
 
-fn solvable(res: Num, term: &Term, fixed_until: usize) -> bool {
+fn mul_solvable(res: Num, term: &Term, fixed_until: usize) -> bool {
     let current_res = calculate(&term);
     let _debug_term = term.iter().map(|(op, num)| format!(" {} {}", op, num)).join("");
     if current_res == res {
@@ -58,7 +81,7 @@ fn solvable(res: Num, term: &Term, fixed_until: usize) -> bool {
             for i in ones {
                 let mut term = term.clone();
                 term[i].0 = Op::Mul;
-                if solvable(res, &term, fixed_until) {
+                if mul_solvable(res, &term, fixed_until) {
                     return true;
                 }
             }
@@ -74,7 +97,48 @@ fn solvable(res: Num, term: &Term, fixed_until: usize) -> bool {
             }
             let mut term = term.clone();
             term[i].0 = Op::Mul;
-            if solvable(res, &term, i + 1) {
+            if mul_solvable(res, &term, i + 1) {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+fn con_mul_solvable(res: Num, term: &Term, fixed_until: usize) -> bool {
+    let current_res = calculate(&term);
+    let _debug_term = term.iter().map(|(op, num)| format!(" {} {}", op, num)).join("");
+    if current_res == res {
+        // eprintln!("{_debug_term} = {res}");
+        true
+    } else if current_res > res && calculate(&term[..fixed_until]) != 1 {
+        // eprintln!("{_debug_term} > {res}");
+        let ones = term.iter().enumerate().skip(fixed_until).filter(|(_i, part)| part.1 == 1 && part.0 == Op::Add).map(|i| i.0).collect::<Vec<_>>();
+        if ones.len() as Num >= current_res - res {
+            for i in ones {
+                let mut term = term.clone();
+                term[i].0 = Op::Mul;
+                if con_mul_solvable(res, &term, fixed_until) {
+                    return true;
+                }
+            }
+            false
+        } else {
+            false
+        }
+    } else {
+        // eprintln!("{_debug_term} < {res}");
+        for i in fixed_until..term.len() {
+            if term[i].0 == Op::Mul {
+                continue
+            }
+            let mut term = term.clone();
+            term[i].0 = Op::Mul;
+            if con_mul_solvable(res, &term, i + 1) {
+                return true;
+            }
+            term[i].0 = Op::Con;
+            if con_mul_solvable(res, &term, i + 1) {
                 return true;
             }
         }
@@ -83,19 +147,23 @@ fn solvable(res: Num, term: &Term, fixed_until: usize) -> bool {
 }
 
 fn calculate(term: &[(Op, Num)]) -> Num {
-    let init = match term[0].0 {
-        Op::Add => 0,
-        Op::Mul => 1,
-    };
-    term.iter().fold(init, |acc, (op, n)| match op {
-        Op::Add => acc.checked_add(*n).unwrap(),
-        Op::Mul => acc.checked_mul(*n).unwrap(),
-    })
+    let init = term[0].0.identity();
+    term.iter().fold(init, |acc, (op, n)| op.apply(acc, *n))
 }
 
 #[aoc(day7, part2)]
 fn two(input: &Input) -> Output {
-    todo!()
+    let mut answer = 0;
+    for (res, numbers) in input {
+        let term = numbers.iter().map(|i| {
+            assert!(*i > 0);
+            (Op::Add, *i)
+        }).collect::<Vec<_>>();
+        if con_mul_solvable(*res, &term, 1) {
+            answer += res;
+        }
+    }
+    answer
 }
 
 
@@ -117,10 +185,10 @@ mod examples {
         assert_eq!(res, 3749);
     }
 
-    // #[test]
-    // fn example2() {
-    //     let res = part2(include_str!("test.txt"));
-    //     assert_eq!(res, todo!());
-    // }
+    #[test]
+    fn example2() {
+        let res = part2(include_str!("test.txt"));
+        assert_eq!(res, 11387);
+    }
 
 }
