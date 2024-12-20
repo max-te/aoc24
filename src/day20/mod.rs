13f6f232dfe72f2a8997e468d82c77739f46755d
Max Teegen <m_teegen@gmx.de>
2024-12-20 16:23:08 +0100

    day 20 done

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -125,8 +125,94 @@ fn one_dual(input: &Input) -> usize {
     one_inner_dual(input, 100)
 }
 
+const fn manhattan_diamond<const RADIUS: usize, const LEN: usize>() -> [(isize, isize); LEN] {
+    assert!(LEN == 4 * RADIUS);
+    let mut i: usize = 0;
+    let mut out = [(0, 0); LEN];
+    while i < RADIUS {
+        out[i] = (i as isize, (RADIUS - i) as isize);
+        out[i + RADIUS] = ((RADIUS - i) as isize, -(i as isize));
+        out[i + 2 * RADIUS] = (-(i as isize), -((RADIUS - i) as isize));
+        out[i + 3 * RADIUS] = (-((RADIUS - i) as isize), i as isize);
+        i += 1;
+    }
+    out
+}
+
+macro_rules! make_manhattan_diamond {
+    ($radius:expr) => {
+        manhattan_diamond::<$radius, { 4 * $radius }>()
+    };
+}
+
+const MANHATTAN_DIAMONDS: [&[(isize, isize)]; 21] = [
+    &[(0, 0)],
+    &make_manhattan_diamond!(1),
+    &make_manhattan_diamond!(2),
+    &make_manhattan_diamond!(3),
+    &make_manhattan_diamond!(4),
+    &make_manhattan_diamond!(5),
+    &make_manhattan_diamond!(6),
+    &make_manhattan_diamond!(7),
+    &make_manhattan_diamond!(8),
+    &make_manhattan_diamond!(9),
+    &make_manhattan_diamond!(10),
+    &make_manhattan_diamond!(11),
+    &make_manhattan_diamond!(12),
+    &make_manhattan_diamond!(13),
+    &make_manhattan_diamond!(14),
+    &make_manhattan_diamond!(15),
+    &make_manhattan_diamond!(16),
+    &make_manhattan_diamond!(17),
+    &make_manhattan_diamond!(18),
+    &make_manhattan_diamond!(19),
+    &make_manhattan_diamond!(20),
+];
+
+fn two_inner((map, start, finish): &Input, min_save: usize) -> usize {
+    let mut map = map.clone();
+    let track = bfs(
+        start,
+        |t| {
+            map.neighbours(*t, false)
+                .filter(|t| matches!(map[*t], Tile::Track(_)))
+        },
+        |t| t == finish,
+    )
+    .unwrap();
+    let mut shortcuts = 0;
+
+    for (time, pos) in track.iter().enumerate() {
+        map[*pos] = Tile::Track(time);
+        for cheat_duration in 2..=20 {
+            for target in MANHATTAN_DIAMONDS[cheat_duration]
+                .iter()
+                .filter_map(|dir| map.move_in_direction(*pos, *dir))
+            {
+                if let Tile::Track(target_time) = map[target] {
+                    if time.saturating_sub(target_time) >= cheat_duration + min_save {
+                        #[cfg(test)]
+                        eprintln!(
+                        "{cheat_duration}-Shortcut to {pos:?} from {target:?} to time {time} from {target_time}"
+                    );
+                        shortcuts += 1;
+                    };
+                }
+            }
+        }
+    }
+    shortcuts
+}
+
+#[aoc(day20, part2, naive)]
+fn two(input: &Input) -> usize {
+    two_inner(input, 100)
+}
+
 #[cfg(test)]
 mod examples {
+    use itertools::Itertools;
+
     use super::*;
 
     #[test]
@@ -139,5 +225,24 @@ mod examples {
     fn example1_dual() {
         let res = one_inner_dual(&parse(include_str!("test.txt")), 20);
         assert_eq!(res, 5);
+    }
+
+    #[test]
+    fn example2() {
+        let res = two_inner(&parse(include_str!("test.txt")), 50);
+        assert_eq!(
+            res,
+            [32, 31, 29, 39, 25, 23, 20, 19, 12, 14, 12, 22, 4, 3]
+                .iter()
+                .sum()
+        );
+    }
+
+    #[test]
+    fn manhattan_test() {
+        assert_eq!(
+            manhattan_diamond::<2, 8>().iter().sorted().collect_vec(),
+            SHORTCUT_DIRECTIONS.iter().sorted().collect_vec()
+        );
     }
 }
