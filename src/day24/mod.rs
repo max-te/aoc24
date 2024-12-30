a4fe414eb3c86a27d49f3a72a8894ab1e04def48
Max Teegen <m_teegen@gmx.de>
2024-12-30 12:32:19 +0100

    day 24: sized wirenames

--- src/day24/mod.rs
+++ src/day24/mod.rs
@@ -1,10 +1,10 @@
-use std::{cell::RefCell, ops::Deref};
+use std::{cell::RefCell, ops::Deref, str};
 
 use aoc_runner_derive::aoc;
 use itertools::Itertools;
 use rustc_hash::{FxHashMap, FxHashSet};
 
-type WireName = String;
+type WireName = [u8; 3];
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum WireValue {
     Determined(bool),
@@ -22,27 +22,32 @@ fn parse(input: &str) -> (FxHashMap<WireName, RefCell<WireValue>>, i8) {
             break;
         }
         let (wire, value) = line.split_once(": ").unwrap();
-        if wire.starts_with("z") {
+        let wire = wire.as_bytes();
+        let wire = [wire[0], wire[1], wire[2]];
+        if wire.starts_with(b"z") {
             z_index += 1;
         }
-        wires.insert(
-            wire.to_string(),
-            RefCell::new(WireValue::Determined(value == "1")),
-        );
+        wires.insert(wire, RefCell::new(WireValue::Determined(value == "1")));
     }
     for line in lines {
         let (rule, wire) = line.split_once(" -> ").unwrap();
+        let wire = wire.as_bytes();
+        let wire = [wire[0], wire[1], wire[2]];
         let (a, operand, b) = rule.split(' ').collect_tuple().unwrap();
         let (a, b) = if a < b { (a, b) } else { (b, a) };
-        if wire.starts_with("z") {
+        let a = a.as_bytes();
+        let b = b.as_bytes();
+        let a = [a[0], a[1], a[2]];
+        let b = [b[0], b[1], b[2]];
+        if wire.starts_with(b"z") {
             z_index += 1;
         }
         wires.insert(
-            wire.to_string(),
+            wire,
             RefCell::new(match operand {
-                "AND" => WireValue::GateAnd(a.to_string(), b.to_string()),
-                "OR" => WireValue::GateOr(a.to_string(), b.to_string()),
-                "XOR" => WireValue::GateXor(a.to_string(), b.to_string()),
+                "AND" => WireValue::GateAnd(a, b),
+                "OR" => WireValue::GateOr(a, b),
+                "XOR" => WireValue::GateXor(a, b),
                 _ => unreachable!(),
             }),
         );
@@ -51,14 +56,19 @@ fn parse(input: &str) -> (FxHashMap<WireName, RefCell<WireValue>>, i8) {
     (wires, z_index)
 }
 
+#[inline]
+fn numbered_wire(prefix: u8, index: i8) -> WireName {
+    [prefix, b'0' + (index / 10) as u8, b'0' + (index % 10) as u8]
+}
+
 #[aoc(day24, part1)]
 pub fn part1(input: &str) -> usize {
-    let (mut wires, mut z_index) = parse(input);
+    let (wires, mut z_index) = parse(input);
 
     let mut res: usize = 0;
     while z_index >= 0 {
-        let z_wire = format!("z{:02}", z_index);
-        let z = determine_wire_value(&wires, z_wire).unwrap();
+        let z_wire = numbered_wire(b'z', z_index);
+        let z = determine_wire_value(&wires, &z_wire).unwrap();
         res = (res << 1) | if z { 1 } else { 0 };
         z_index -= 1;
     }
@@ -68,14 +78,13 @@ pub fn part1(input: &str) -> usize {
 
 fn determine_wire_value(
     wires: &FxHashMap<WireName, RefCell<WireValue>>,
-    wire_name: impl AsRef<str>,
+    wire_name: &WireName,
 ) -> Result<bool, WireName> {
-    let wire_name = wire_name.as_ref();
     let mut cell = wires
         .get(wire_name)
-        .ok_or_else(|| wire_name.to_owned())?
+        .ok_or_else(|| wire_name.clone())?
         .try_borrow_mut()
-        .or_else(|_| Err(wire_name.to_owned()))?;
+        .or_else(|_| Err(wire_name.clone()))?;
     match cell.deref() {
         WireValue::Determined(value) => Ok(*value),
         WireValue::GateAnd(a, b) => {
@@ -108,7 +117,7 @@ pub fn part2(input: &str) -> String {
     // let mut expected_inputs = FxHashSet::default();
     let mut wrong_gates = FxHashSet::default();
     for z in 0..z_max {
-        let z_wire = format!("z{:02}", z);
+        let z_wire = numbered_wire(b'z', z);
         // expected_inputs.insert(format!("x{:02}", z));
         // expected_inputs.insert(format!("y{:02}", z));
         // let inputs = controlling_inputs(&wires, &z_wire);
@@ -118,17 +127,20 @@ pub fn part2(input: &str) -> String {
 
         let z_gate = wires.get(&z_wire).unwrap().borrow();
         if !matches!(z_gate.deref(), WireValue::GateXor(_, _)) {
-            eprintln!("Expected xor for {z_wire}, got {:?}", z_gate.deref());
+            #[cfg(debug_assertions)]
+            eprintln!("Expected xor for {z_wire:?}, got {:?}", z_gate.deref());
             wrong_gates.insert(z_wire);
         }
     }
-    let z_max_wire = format!("z{:02}", z_max);
+    let z_max_wire = numbered_wire(b'z', z_max);
+    #[cfg(debug_assertions)]
     dbg!(&z_max_wire);
     {
         let z_max_gate = wires.get(&z_max_wire).unwrap().borrow();
         if !matches!(z_max_gate.deref(), WireValue::GateOr(_, _)) {
+            #[cfg(debug_assertions)]
             eprintln!(
-                "Exptected carry {z_max_wire} to be an or gate, was {:?}",
+                "Exptected carry {z_max_wire:?} to be an or gate, was {:?}",
                 z_max_gate.deref()
             );
             wrong_gates.insert(z_max_wire.clone());
@@ -138,40 +150,42 @@ pub fn part2(input: &str) -> String {
         let gate = gate.borrow();
         match gate.deref() {
             WireValue::Determined(_) => {
-                if !wire.starts_with("x") && !wire.starts_with("y") {
-                    eprintln!("Did not expect {wire} to be pre-determined.");
+                if !wire.starts_with(b"x") && !wire.starts_with(b"y") {
+                    #[cfg(debug_assertions)]
+                    eprintln!("Did not expect {wire:?} to be pre-determined.");
                     wrong_gates.insert(wire.clone());
                 }
             }
             WireValue::GateAnd(a, b) => {
-                if a != "x00" {
+                if a != b"x00" {
                     if !wires.values().any(|v| {
                         let v = v.borrow();
                         matches!(v.deref(), WireValue::GateOr(a2, b2) if a2 == wire || b2 == wire)
                     }) {
-                        eprintln!("Expected {gate:?} at {wire} to feed into or gate.");
+                        #[cfg(debug_assertions)]
+                        eprintln!("Expected {gate:?} at {wire:?} to feed into or gate.");
                         wrong_gates.insert(wire.clone());
                     }
                 }
-                if !a.starts_with("x") {
+                if !a.starts_with(b"x") {
                     let a_gate = wires.get(a).unwrap().borrow();
                     let b_gate = wires.get(b).unwrap().borrow();
                     if !(matches!(
                         a_gate.deref(),
                         WireValue::GateXor(_, _) | WireValue::GateOr(_, _)
-                    ) || a_gate.deref()
-                        == &WireValue::GateAnd(String::from("x00"), String::from("y00")))
+                    ) || a_gate.deref() == &WireValue::GateAnd(*b"x00", *b"y00"))
                     {
-                        eprintln!("Expected {gate:?} at {wire} input {a} to be a xor or or gate, was {:?}.", a_gate.deref());
+                        #[cfg(debug_assertions)]
+                        eprintln!("Expected {gate:?} at {wire:?} input {a:?} to be a xor or or gate, was {:?}.", a_gate.deref());
                         wrong_gates.insert(a.clone());
                     }
                     if !(matches!(
                         b_gate.deref(),
                         WireValue::GateXor(_, _) | WireValue::GateOr(_, _)
-                    ) || b_gate.deref()
-                        == &WireValue::GateAnd(String::from("x00"), String::from("y00")))
+                    ) || b_gate.deref() == &WireValue::GateAnd(*b"x00", *b"y00"))
                     {
-                        eprintln!("Expected {gate:?} at {wire} input {b} to be a xor or or gate, was {:?}.", b_gate.deref());
+                        #[cfg(debug_assertions)]
+                        eprintln!("Expected {gate:?} at {wire:?} input {b:?} to be a xor or or gate, was {:?}.", b_gate.deref());
                         wrong_gates.insert(b.clone());
                     }
                 }
@@ -182,30 +196,34 @@ pub fn part2(input: &str) -> String {
                         let v = v.borrow();
                         matches!(v.deref(), WireValue::GateAnd(a2, b2) if a2 == wire || b2 == wire)
                     }) {
-                        eprintln!("Expected {gate:?} at {wire} to feed into last z or and gate.");
+                        #[cfg(debug_assertions)]
+                        eprintln!("Expected {gate:?} at {wire:?} to feed into last z or and gate.");
                         wrong_gates.insert(wire.clone());
                     }
                     if !wires.values().any(|v| {
                         let v = v.borrow();
                         matches!(v.deref(), WireValue::GateXor(a2, b2) if a2 == wire || b2 == wire)
                     }) {
-                        eprintln!("Expected {gate:?} at {wire} to feed into last z or xor gate.");
+                        #[cfg(debug_assertions)]
+                        eprintln!("Expected {gate:?} at {wire:?} to feed into last z or xor gate.");
                         wrong_gates.insert(wire.clone());
                     }
                 }
-                if a != "x00" {
+                if a != b"x00" {
                     let a_gate = wires.get(a).unwrap().borrow();
                     let b_gate = wires.get(b).unwrap().borrow();
                     if !matches!(a_gate.deref(), WireValue::GateAnd(_, _)) {
+                        #[cfg(debug_assertions)]
                         eprintln!(
-                            "Expected {gate:?} at {wire} input {a} to be an and gate, was {:?}.",
+                            "Expected {gate:?} at {wire:?} input {a:?} to be an and gate, was {:?}.",
                             a_gate.deref()
                         );
                         wrong_gates.insert(a.clone());
                     }
                     if !matches!(b_gate.deref(), WireValue::GateAnd(_, _)) {
+                        #[cfg(debug_assertions)]
                         eprintln!(
-                            "Expected {gate:?} at {wire} input {b} to be an and gate, was {:?}.",
+                            "Expected {gate:?} at {wire:?} input {b:?} to be an and gate, was {:?}.",
                             b_gate.deref()
                         );
                         wrong_gates.insert(b.clone());
@@ -213,35 +231,39 @@ pub fn part2(input: &str) -> String {
                 }
             }
             WireValue::GateXor(a, b) => {
-                if !wire.starts_with("z") {
+                if !wire.starts_with(b"z") {
                     if !wires.values().any(|v| {
                         let v = v.borrow();
                         matches!(v.deref(), WireValue::GateAnd(a2, b2) if a2 == wire || b2 == wire)
                     }) {
-                        eprintln!("Expected {gate:?} at {wire} to feed into z-wire or and gate.");
+                        #[cfg(debug_assertions)]
+                        eprintln!("Expected {gate:?} at {wire:?} to feed into z-wire or and gate.");
                         wrong_gates.insert(wire.clone());
                     }
                     if !wires.values().any(|v| {
                         let v = v.borrow();
                         matches!(v.deref(), WireValue::GateXor(a2, b2) if a2 == wire || b2 == wire)
                     }) {
-                        eprintln!("Expected {gate:?} at {wire} to feed into z-wire or xor gate.");
+                        #[cfg(debug_assertions)]
+                        eprintln!("Expected {gate:?} at {wire:?} to feed into z-wire or xor gate.");
                         wrong_gates.insert(wire.clone());
                     }
-                    if !a.starts_with("x") {
+                    if !a.starts_with(b"x") {
                         let a_gate = wires.get(a).unwrap().borrow();
                         let b_gate = wires.get(b).unwrap().borrow();
 
                         match a_gate.deref() {
                             WireValue::GateOr(a2, _) | WireValue::GateXor(a2, _) => {
-                                if wire.starts_with("z") {
-                                    if a2.starts_with("x") {
-                                        eprintln!("Expected {gate:?} at {wire} input {a} to be a 2nd order gate, was {a_gate:?}");
+                                if wire.starts_with(b"z") {
+                                    if a2.starts_with(b"x") {
+                                        #[cfg(debug_assertions)]
+                                        eprintln!("Expected {gate:?} at {wire:?} input {a:?} to be a 2nd order gate, was {a_gate:?}");
                                         wrong_gates.insert(wire.clone());
                                     }
                                 } else {
-                                    if !a2.starts_with("x") {
-                                        eprintln!("Expected {gate:?} at {wire} input {a} to be a 1st order gate, was {a_gate:?}");
+                                    if !a2.starts_with(b"x") {
+                                        #[cfg(debug_assertions)]
+                                        eprintln!("Expected {gate:?} at {wire:?} input {a:?} to be a 1st order gate, was {a_gate:?}");
                                         wrong_gates.insert(wire.clone());
                                     }
                                 }
@@ -252,26 +274,27 @@ pub fn part2(input: &str) -> String {
                         if !(matches!(
                             a_gate.deref(),
                             WireValue::GateXor(_, _) | WireValue::GateOr(_, _)
-                        ) || a_gate.deref()
-                            == &WireValue::GateAnd(String::from("x00"), String::from("y00")))
+                        ) || a_gate.deref() == &WireValue::GateAnd(*b"x00", *b"y00"))
                         {
-                            eprintln!("Expected {gate:?} at {wire} input {a} to be a xor or or gate, was {:?}.", a_gate.deref());
+                            #[cfg(debug_assertions)]
+                            eprintln!("Expected {gate:?} at {wire:?} input {a:?} to be a xor or or gate, was {:?}.", a_gate.deref());
                             wrong_gates.insert(a.clone());
                         }
 
                         if !(matches!(
                             b_gate.deref(),
                             WireValue::GateXor(_, _) | WireValue::GateOr(_, _)
-                        ) || b_gate.deref()
-                            == &WireValue::GateAnd(String::from("x00"), String::from("y00")))
+                        ) || b_gate.deref() == &WireValue::GateAnd(*b"x00", *b"y00"))
                         {
-                            eprintln!("Expected {gate:?} at {wire} input {b} to be a xor or or gate, was {:?}.", b_gate.deref());
+                            #[cfg(debug_assertions)]
+                            eprintln!("Expected {gate:?} at {wire:?} input {b:?} to be a xor or or gate, was {:?}.", b_gate.deref());
                             wrong_gates.insert(b.clone());
                         }
                     }
-                } else if wire != "z00" {
-                    if a.starts_with("x") || b.starts_with("y") {
-                        eprintln!("Did not expect {gate:?} at {wire} to feed into z-wire.");
+                } else if wire != b"z00" {
+                    if a.starts_with(b"x") || b.starts_with(b"y") {
+                        #[cfg(debug_assertions)]
+                        eprintln!("Did not expect {gate:?} at {wire:?} to feed into z-wire.");
                         wrong_gates.insert(wire.clone());
                     }
                 }
@@ -279,14 +302,24 @@ pub fn part2(input: &str) -> String {
         }
     }
 
-    wrong_gates.iter().sorted().join(",")
+    let out =
+        wrong_gates
+            .iter()
+            .sorted()
+            .fold(Vec::with_capacity(wrong_gates.len() * 4), |mut f, g| {
+                f.extend(g);
+                f.push(b',');
+                f
+            });
+    let mut out = unsafe { String::from_utf8_unchecked(out) };
+    out.pop();
+    out
 }
 
 fn controlling_inputs(
     wires: &FxHashMap<WireName, RefCell<WireValue>>,
-    wire_name: impl AsRef<str>,
+    wire_name: &WireName,
 ) -> FxHashSet<WireName> {
-    let wire_name = wire_name.as_ref();
     match wires.get(wire_name).unwrap().borrow().deref() {
         WireValue::GateAnd(a, b) | WireValue::GateOr(a, b) | WireValue::GateXor(a, b) => {
             let a_inputs = controlling_inputs(wires, a);
@@ -295,7 +328,7 @@ fn controlling_inputs(
             inputs.extend(b_inputs);
             inputs
         }
-        WireValue::Determined(_) => FxHashSet::from_iter([wire_name.to_string()]),
+        WireValue::Determined(_) => FxHashSet::from_iter([*wire_name]),
     }
 }
 
